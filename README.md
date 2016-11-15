8b9f5e291d92337eb0add3edb39b51935d6c6f65
Stephen Street <stephen@redrocketcomputing.com>
2016-11-15 13:37:13 -0800

    Moving way from this to teleop_tools

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # teleop
 Blue-Slam Teleop Package
+
+# PS4 Linux Button Mapping
+-  0: Square
+-  1: X
+-  2: Circle
+-  3: Triangle
+-  4: L1
+-  5: R1
+-  6: L2
+-  7: R2
+-  8: Share
+-  9: Options
+- 10: Left Stick
+- 11: Right Stick
+- 12: PS
+- 13: Touchpad
+
+# PS4 Linux Axis Mapping
+-  0: Left Stick Horizontal
+-  1: Left Stick Vertical
+-  2: Right Stick Horizontal
+-  3: L2
+-  4: R3
+-  5: Right Stick Vertical
+-  6: Left/Right Arrows
+-  7: Up/Down Arrows
+-  8: 
+-  9: Touch Pad
+- 10: Touch Pad
+- 11:
--- src/controller/_teleop_controller.py
+++ src/controller/_teleop_controller.py
@@ -7,20 +7,19 @@ from geometry_msgs.msg import Twist
 
 class TeleopController:
 
-    def __init__(self, params):
+    def __init__(self, robot_name):
         self._twist = Twist()
         self._enabled = False
 
-        name = params['lowlevel_name']
-        self._vel_axis = params['vel_axis']
-        self._omega_axis = params['omega_axis']
-        self._enable_button = params['enable_button']
+        self._vel_axis = rospy.get_param('~vel_axis')
+        self._omega_axis = rospy.get_param('~omega_axis')
+        self._enable_button = rospy.get_param('~enable_button')
 
-        max_rpm = rospy.get_param(name + '/motors/max_rpm')
-        r = rospy.get_param(name + '/geometry/r')
-        d = rospy.get_param(name + '/geometry/d')
+        max_rpm = rospy.get_param(robot_name + '/motors/max_rpm')
+        r = rospy.get_param(robot_name + '/geometry/r')
+        d = rospy.get_param(robot_name + '/geometry/d')
         self._max_vel = (np.pi * max_rpm * r) / 30
-        self._max_omega = (np.pi * max_rpm * r) / (15 * d)
+        self._max_omega = ((np.pi * max_rpm * r) / (15 * d)) * .50
 
         self._twist_pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
         self._joy_sub = rospy.Subscriber('joy', Joy, self._joy_handler)
@@ -31,7 +30,7 @@ class TeleopController:
         self._twist.linear.x = msg.axes[self._vel_axis] * self._max_vel
         self._twist.angular.z = msg.axes[self._omega_axis] * self._max_omega
 
-    def drive(self):
+    def update(self):
         if self._enabled:
             self._twist_pub.publish(self._twist)
 
